04c663842194c8792bfd9c20836a2f6318a38f9c
Akorra <neverfearphilishere@gmail.com>
2026-02-02 12:36:57 +0000

    transform logic class

---
+++ include/TLETC/Scene/Transform.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "TLETC/Core/Math.h"
+
+namespace TLETC {
+
+class Transform {
+public:
+    Vec3 position;
+    Quat rotation;
+    Vec3 scale;
+    
+    Transform* parent;
+    
+    Transform()
+        : position(0.0f, 0.0f, 0.0f)
+        , rotation(1.0f, 0.0f, 0.0f, 0.0f)  // Identity quaternion
+        , scale(1.0f, 1.0f, 1.0f)
+        , parent(nullptr)
+    {}
+    
+    // Get the local transformation matrix
+    Mat4 GetModelMatrix() const {
+        Mat4 model = Mat4(1.0f);
+        model = translate(model, position);
+        model = model * glm::mat4_cast(rotation);
+        model = scale(model, this->scale);
+        return model;
+    }
+    
+    // Get the world transformation matrix (including parent transforms)
+    Mat4 GetWorldMatrix() const {
+        Mat4 localMatrix = GetModelMatrix();
+        if (parent) {
+            return parent->GetWorldMatrix() * localMatrix;
+        }
+        return localMatrix;
+    }
+    
+    // Get world position
+    Vec3 GetWorldPosition() const {
+        if (parent) {
+            Mat4 worldMatrix = GetWorldMatrix();
+            return Vec3(worldMatrix[3]);
+        }
+        return position;
+    }
+    
+    // Direction vectors
+    Vec3 GetForward() const {
+        return rotation * Vec3(0.0f, 0.0f, -1.0f);
+    }
+    
+    Vec3 GetRight() const {
+        return rotation * Vec3(1.0f, 0.0f, 0.0f);
+    }
+    
+    Vec3 GetUp() const {
+        return rotation * Vec3(0.0f, 1.0f, 0.0f);
+    }
+    
+    // Rotation helpers
+    void Rotate(const Vec3& axis, float angleDegrees) {
+        rotation = QuatFromAxisAngle(axis, Radians(angleDegrees)) * rotation;
+        rotation = normalize(rotation);
+    }
+    
+    void LookAt(const Vec3& target, const Vec3& up = Vec3(0.0f, 1.0f, 0.0f)) {
+        Vec3 direction = normalize(target - position);
+        Mat4 lookMatrix = glm::lookAt(position, target, up);
+        rotation = glm::quat_cast(glm::inverse(lookMatrix));
+    }
+    
+    // Hierarchy
+    void SetParent(Transform* newParent) {
+        parent = newParent;
+    }
+};
+
+} // namespace TLETC
