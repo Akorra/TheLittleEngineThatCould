d48015e9a686c3e426393643bcc2e8a27956066c
Akorra <neverfearphilishere@gmail.com>
2026-02-05 23:47:57 +0000

    perspective and ortographic camera support

--- include/TLETC/Rendering/Camera.h
+++ include/TLETC/Rendering/Camera.h
@@ -6,17 +6,29 @@
 namespace TLETC {
 
 /**
- * Camera - Provides view and projection matrices for rendering
+ * Camera - Base camera class
+ * 
+ * Provides view and projection matrices for rendering.
+ * Can be perspective or orthographic.
  * 
  * Usage:
  *   Camera camera;
  *   camera.position = Vec3(0, 5, 10);
  *   camera.target = Vec3(0, 0, 0);
+ *   camera.SetPerspective(45.0f, 0.1f, 100.0f);
  *   app.SetCamera(&camera);
  */
 class Camera
 {
 public:
+    enum class ProjectionType { Perspective, Orthographic };
+
+    Camera();
+    virtual ~Camera() = default;
+
+    // Projection type
+    ProjectionType projectionType = ProjectionType::Perspective;
+
     // Transform
     Vec3 position = Vec3(0, 0, 5);
     Vec3 target = Vec3(0, 0, 0);
@@ -27,9 +39,18 @@ public:
     float nearPlane = 0.1f;
     float farPlane = 100.0f;
     
+    // Orthographic parameters
+    float orthoSize = 10.0f;  // Half-height of view volume
+    
     // Get matrices
-    Mat4 GetViewMatrix() const;
-    Mat4 GetProjectionMatrix(float aspectRatio) const;
+    virtual Mat4 GetViewMatrix() const;
+    virtual Mat4 GetProjectionMatrix(float aspectRatio) const;
+    
+    // Helpers
+    void SetPerspective() { projectionType = ProjectionType::Perspective;  }; //< use stored values
+    void SetOrtographic() { projectionType = ProjectionType::Orthographic; }; //< use stored values
+    void SetPerspective(float fovDegrees, float near, float far);
+    void SetOrthographic(float size, float near, float far);
 };
 
 } // namespace TLETC
--- src/Rendering/Camera.cpp
+++ src/Rendering/Camera.cpp
@@ -10,7 +10,34 @@ Mat4 Camera::GetViewMatrix() const
 
 Mat4 Camera::GetProjectionMatrix(float aspectRatio) const 
 {
-    return perspective(radians(fov), aspectRatio, nearPlane, farPlane);
+    if (projectionType == ProjectionType::Perspective) 
+    {
+        return perspective(radians(fov), aspectRatio, nearPlane, farPlane);
+    } 
+    else 
+    {
+        // Orthographic
+        float halfWidth = orthoSize * aspectRatio;
+        float halfHeight = orthoSize;
+        return ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, nearPlane, farPlane);
+    }
+}
+
+void Camera::SetPerspective(float fovDegrees, float near, float far) 
+{
+    projectionType = ProjectionType::Perspective;
+
+    fov = fovDegrees;
+    nearPlane = near;
+    farPlane = far;
+}
+
+void Camera::SetOrthographic(float size, float near, float far) 
+{
+    projectionType = ProjectionType::Orthographic;
+    orthoSize = size;
+    nearPlane = near;
+    farPlane = far;
 }
 
 } // namespace TLETC
