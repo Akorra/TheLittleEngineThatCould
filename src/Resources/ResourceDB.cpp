a58209e2068eab192ab553ae9caf75755060ba45
Akorra <neverfearphilishere@gmail.com>
2026-02-12 12:58:36 +0000

    restructuring

--- src/Resources/ResourceDB.cpp
+++
@@ -1,9 +0,0 @@
-#include "TLETC/Resources/ResourceDB.h"
-
-// Resources
-namespace TLETC::Resources
-{
-MeshManager     Meshes;
-MaterialManager Materials;
-TextureManager  Textures;
-} // namespace TLETC::Resources
