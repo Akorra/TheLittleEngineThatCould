c43cc4e394637bf6d179c59cd1f52b6fd0357c39
Akorra <neverfearphilishere@gmail.com>
2026-02-12 22:38:16 +0000

    update example for application, window and input testing

--- examples/01_HelloTriangle/main.cpp
+++ examples/01_HelloTriangle/main.cpp
@@ -1,9 +1,48 @@
 #include "TLETC/Core/Application.h"
+#include "TLETC/Core/Log.h"
 
-int main() 
+class TestApp : public TLETC::Application
 {
-    TLETC::Application app("[TLETC] 01_HelloTriangle!");
-    app.Run();
+public:
+    TestApp() : Application("Milestone 1 Test") {}
+    
+    void OnStartup() override
+    {
+        TLETC_INFO("Game started!");
+    }
+    
+    void OnUpdate(float dt) override
+    {
+        // Test input
+        if (input_->IsKeyPressed(TLETC::KeyCode::Escape))
+        {
+            TLETC_INFO("Escape pressed - exiting");
+            RequestShutdown(); // This will end the loop
+        }
+        
+        if (input_->IsKeyDown(TLETC::KeyCode::W))
+            TLETC_TRACE("W held");
+        
+        if (input_->IsMouseButtonPressed(TLETC::MouseButton::Left))
+        {
+            auto pos = input_->GetMousePosition();
+            TLETC_INFO("Mouse clicked at: ", pos.x, ", ", pos.y);
+        }
+    }
+    
+    void OnRender() override
+    {
+    }
+    
+    void OnShutdown() override
+    {
+        TLETC_INFO("Game shutdown");
+    }
+};
 
+int main()
+{
+    TestApp app;
+    app.Run();
     return 0;
 }
