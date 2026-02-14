9413320e4776184ec0d52fef476028cbd6d11f0f
Akorra <neverfearphilishere@gmail.com>
2026-02-14 15:35:02 +0000

    double ended stack allocator

--- src/Core/Memory/StackAllocator.cpp
+++ src/Core/Memory/StackAllocator.cpp
@@ -132,22 +132,76 @@ void DoubleEndedStackAllocator::Reset()
 
 void* DoubleEndedStackAllocator::AllocateBottom(size_t size, size_t alignment)
 {
-    
+    // Calculate header + data size
+    size_t headerSize = sizeof(AllocationHeader);
+    size_t totalSize = headerSize + size;
+
+    auto alignedOffset = AlignSize(bottomOffset_, alignment);
+    if(alignedOffset + totalSize > topOffset_)
+    {
+        TLETC_ERROR("StackAllocator out of memory!");
+        return nullptr;
+    }
+
+    AllocationHeader* header = reinterpret_cast<AllocationHeader*>(static_cast<char*>(buffer_) + alignedOffset);
+    header->prevOffset = bottomPrevOffset_;
+    header->size = size;
+
+    bottomPrevOffset_ = alignedOffset;
+    bottomOffset_     = alignedOffset + totalSize;  
+
+    return header + 1;
 }
 
 void* DoubleEndedStackAllocator::AllocateTop(size_t size, size_t alignment)
 {
+    // Calculate header + data size
+    size_t dataOffset = topOffset_ - size;
+    size_t alignedOffset = dataOffset & ~(alignment - 1); // round offset down to the closest alignment that fits our memory
+    size_t headerOffset = alignedOffset - sizeof(AllocationHeader);
+
+    // Check space
+    if (headerOffset < bottomOffset_)
+    {
+        TLETC_ERROR("StackAllocator out of memory!");
+        return nullptr;
+    }
+    
+    AllocationHeader* header = reinterpret_cast<AllocationHeader*>(static_cast<char*>(buffer_) + headerOffset);
+    header->prevOffset = topPrevOffset_;
+    header->size = size;
+
+    topPrevOffset_ = headerOffset;
+    topOffset_     = headerOffset;  
+
+    return header + 1;
 
 }
 
 void DoubleEndedStackAllocator::FreeBottom(void* ptr)
 {
-
+    if (!ptr) return;
+    
+    AllocationHeader* header = static_cast<AllocationHeader*>(ptr) - 1;
+    
+    size_t headerOffset = reinterpret_cast<char*>(header) - static_cast<char*>(buffer_);
+    TLETC_ASSERT(headerOffset == bottomPrevOffset_, "Bottom stack: must free in LIFO order!");
+    
+    bottomOffset_ = bottomPrevOffset_;
+    bottomPrevOffset_ = header->prevOffset;
 }
 
 void DoubleEndedStackAllocator::FreeTop(void* ptr)
 {
-
+    if (!ptr) return;
+    
+    AllocationHeader* header = static_cast<AllocationHeader*>(ptr) - 1;
+    
+    size_t headerOffset = reinterpret_cast<char*>(header) - static_cast<char*>(buffer_);
+    TLETC_ASSERT(headerOffset == topPrevOffset_, "Top stack: must free in LIFO order!");
+    
+    topOffset_ = topPrevOffset_ + sizeof(AllocationHeader) + header->size;
+    topPrevOffset_ = header->prevOffset;
 }  
 
 void DoubleEndedStackAllocator::ResetBottom()
